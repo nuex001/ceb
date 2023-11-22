73fad4be7e415c175f8ff7d337ccb4090cb1d621
nuex001 <edeemmanuelchizurumoke@gmail.com>
2023-11-22 07:29:20 +0100

    second commit. refactor almost everything

--- main.js
+++ main.js
@@ -5,40 +5,40 @@ const addAnimations = () => {
     const merch = document.getElementById('merch');
 
     const triggerbottom = window.innerHeight / 5 * 4;
-    if (about.getBoundingClientRect().top < triggerbottom) {
+    if (home.getBoundingClientRect().top < triggerbottom) {
         // console.log("reached");
-        about.classList.add("active");
+        home.classList.add("active");
     } else {
-        about.classList.remove("active");
+        home.classList.remove("active");
     }
 
-    if (service.getBoundingClientRect().top < triggerbottom) {
-        // console.log("reached");
-        service.classList.add("active");
-    } else {
-        service.classList.remove("active");
-    }
+    // if (service.getBoundingClientRect().top < triggerbottom) {
+    //     // console.log("reached");
+    //     service.classList.add("active");
+    // } else {
+    //     service.classList.remove("active");
+    // }
 
-    if (blog.getBoundingClientRect().top < triggerbottom) {
-        // console.log("reached");
-        blog.classList.add("active");
-    } else {
-        blog.classList.remove("active");
-    }
+    // if (blog.getBoundingClientRect().top < triggerbottom) {
+    //     // console.log("reached");
+    //     blog.classList.add("active");
+    // } else {
+    //     blog.classList.remove("active");
+    // }
 
-    if (merch.getBoundingClientRect().top < triggerbottom) {
-        // console.log("reached");
-        merch.classList.add("active");
-    } else {
-        merch.classList.remove("active");
-    }
+    // if (merch.getBoundingClientRect().top < triggerbottom) {
+    //     // console.log("reached");
+    //     merch.classList.add("active");
+    // } else {
+    //     merch.classList.remove("active");
+    // }
 }
-const menuBtn= document.getElementById("menu");
-const ulpop= document.getElementById("ulpop");
+// const menuBtn= document.getElementById("menu");
+// const ulpop= document.getElementById("ulpop");
 
-const togglemenu = () =>{
-    menuBtn.classList.toggle("change")
-    ulpop.classList.toggle("active")
-}
-menuBtn.addEventListener("click", togglemenu);
+// const togglemenu = () =>{
+//     menuBtn.classList.toggle("change")
+//     ulpop.classList.toggle("active")
+// }
+// menuBtn.addEventListener("click", togglemenu);
 window.addEventListener('scroll', addAnimations);
