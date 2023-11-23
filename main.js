a805661eae8c831ab9aa45fcc7afd05ad81062df
nuex001 <edeemmanuelchizurumoke@gmail.com>
2023-11-23 18:11:00 +0100

    added another feature particle js

--- main.js
+++ main.js
@@ -1,39 +1,22 @@
 const addAnimations = () => {
     const home = document.getElementById('sec');
-    // const service = document.getElementById('service');
-    // const blog = document.getElementById('blog');
-    // const merch = document.getElementById('merch');
-
-        // console.log("reached");
         home.classList.add("active");
-
-    // if (service.getBoundingClientRect().top < triggerbottom) {
-    //     // console.log("reached");
-    //     service.classList.add("active");
-    // } else {
-    //     service.classList.remove("active");
-    // }
-
-    // if (blog.getBoundingClientRect().top < triggerbottom) {
-    //     // console.log("reached");
-    //     blog.classList.add("active");
-    // } else {
-    //     blog.classList.remove("active");
-    // }
-
-    // if (merch.getBoundingClientRect().top < triggerbottom) {
-    //     // console.log("reached");
-    //     merch.classList.add("active");
-    // } else {
-    //     merch.classList.remove("active");
-    // }
 }
-// const menuBtn= document.getElementById("menu");
-// const ulpop= document.getElementById("ulpop");
-
-// const togglemenu = () =>{
-//     menuBtn.classList.toggle("change")
-//     ulpop.classList.toggle("active")
-// }
-// menuBtn.addEventListener("click", togglemenu);
+particlesJS('particles-js', {
+    particles: {
+      number: { value: 80, density: { enable: true, value_area: 800 } },
+      color: { value: '#ffffff' },
+      shape: { type: 'circle', stroke: { width: 0, color: '#000000' }, polygon: { nb_sides: 5 }, image: { src: 'img/github.svg', width: 100, height: 100 } },
+      opacity: { value: 0.5, random: false, anim: { enable: false, speed: 1, opacity_min: 0.1, sync: false } },
+      size: { value: 3, random: true, anim: { enable: false, speed: 40, size_min: 0.1, sync: false } },
+      line_linked: { enable: true, distance: 150, color: '#ffffff', opacity: 0.4, width: 1 },
+      move: { enable: true, speed: 6, direction: 'none', random: false, straight: false, out_mode: 'out', bounce: false, attract: { enable: false, rotateX: 600, rotateY: 1200 } }
+    },
+    interactivity: {
+      detect_on: 'canvas',
+      events: { onhover: { enable: true, mode: 'grab' }, onclick: { enable: true, mode: 'push' }, resize: true },
+      modes: { grab: { distance: 140, line_linked: { opacity: 1 } }, bubble: { distance: 400, size: 40, duration: 2, opacity: 8, speed: 3 }, repulse: { distance: 200, duration: 0.4 }, push: { particles_nb: 4 }, remove: { particles_nb: 2 } }
+    },
+    retina_detect: true
+  });
 window.addEventListener('load', addAnimations);
